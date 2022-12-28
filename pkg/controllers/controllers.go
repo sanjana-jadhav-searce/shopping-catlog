10d4a98c6fe459b979578aa857b50960d5933d6f
Sanjana <sanjana.jadhav@searce.com>
2022-12-28 16:26:00 +0530

    Added some APIs

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -17,7 +17,6 @@ import (
 
 var db *sql.DB
 
-
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	db := config.Connect()
 	defer db.Close()
@@ -128,25 +127,54 @@ func DeleteProduct(w http.ResponseWriter, r *http.Request) {
 	db := config.Connect()
 	defer db.Close()
 
-	err := r.ParseMultipartForm(4096)
+	product := r.FormValue("product")
+	rows, err := db.Query("SELECT name, specification, SKU, category, price FROM products WHERE name=?", product)
+	if product == "" {
 
-	if err != nil {
-		panic(err)
+		y := "Data Not Found"
+		json.Marshal(y)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(y)
+		return
 	}
+	if rows.Next() == false {
+		z := "Invalid Product Reference"
+		json.Marshal(z)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(z)
+		return
 
-	name := r.FormValue("name")
-
-	_, err = db.Exec("DELETE FROM products WHERE name=?", name)
-
+	}
+	result, err := db.Exec("DELETE FROM products WHERE name=?", product)
 	if err != nil {
 		log.Print(err)
 		return
 	}
+	rows_affected, err := result.RowsAffected()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if rows_affected != 0 {
+		if err != nil {
+			log.Print(err)
+			return
+		} else {
+			msg := "Product item deleted successfully"
+			json.Marshal(msg)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(msg)
+			return
 
-	x := "Data Deleted successfully!"
-	json.Marshal(x)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(x)
+		}
+	} else {
+		msg := "Product is not found!"
+		json.Marshal(msg)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(msg)
+		return
+
+	}
 }
 
 func CreateInventoryProduct(w http.ResponseWriter, r *http.Request) {
@@ -251,25 +279,180 @@ func DeleteProductInventory(w http.ResponseWriter, r *http.Request) {
 	db := config.Connect()
 	defer db.Close()
 
-	err := r.ParseMultipartForm(4096)
+	product := r.FormValue("product")
+	rows, err := db.Query("SELECT productname, quantity FROM inventories WHERE productname=?", product)
+	if product == "" {
 
+		y := "Data Not Found"
+		json.Marshal(y)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(y)
+		return
+	}
+	if rows.Next() == false {
+		z := "Invalid Inventory Product Reference"
+		json.Marshal(z)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(z)
+		return
+
+	}
+	result, err := db.Exec("DELETE FROM inventories WHERE productname=?", product)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	rows_affected, err := result.RowsAffected()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if rows_affected != 0 {
+		if err != nil {
+			log.Print(err)
+			return
+		} else {
+			msg := "Inventory item deleted successfully"
+			json.Marshal(msg)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(msg)
+			return
+
+		}
+	} else {
+		msg := "Product is not found!"
+		json.Marshal(msg)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(msg)
+		return
+
+	}
+}
+
+func CreateCategory(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+	defer db.Close()
+
+	err := r.ParseMultipartForm(4096)
 	if err != nil {
 		panic(err)
 	}
 
 	name := r.FormValue("name")
 
-	_, err = db.Exec("DELETE FROM inventories WHERE productname=?", name)
+	_, err = db.Exec("INSERT INTO categorymaster(name) VALUES(?)", name)
 
 	if err != nil {
 		log.Print(err)
 		return
 	}
-
-	x := "Data Deleted From Inventory Successfully!"
+	x := "Inserted Category Data Successfully!"
 	json.Marshal(x)
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(x)
+
+}
+
+func GetCategoryByName(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+	defer db.Close()
+	var category models.CategoryMaster
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT name FROM categorymaster WHERE name=?", params["name"])
+	fmt.Println(result)
+	if err != nil {
+		panic(err.Error())
+	}
+	for result.Next() {
+		err := result.Scan(&category.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(category)
+}
+
+func GetCategories(w http.ResponseWriter, r *http.Request) {
+	var categories models.CategoryMaster
+	var category []models.CategoryMaster
+	db := config.Connect()
+	defer db.Close()
+	rows, err := db.Query("SELECT name FROM categorymaster")
+	fmt.Println(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&categories.Name)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			category = append(category, categories)
+		}
+		fmt.Println(category)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(category)
+}
+
+func DeleteCategory(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+	defer db.Close()
+
+	product := r.FormValue("product")
+	rows, err := db.Query("SELECT name FROM categorymaster")
+	if product == "" {
+
+		y := "Data Not Found"
+		json.Marshal(y)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(y)
+		return
+	}
+	if rows.Next() == false {
+		z := "Invalid Category Reference"
+		json.Marshal(z)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(z)
+		return
+
+	}
+	result, err := db.Exec("DELETE FROM categorymaster WHERE product=?", product)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	rows_affected, err := result.RowsAffected()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if rows_affected != 0 {
+		if err != nil {
+			log.Print(err)
+			return
+		} else {
+			msg := "Category item deleted successfully"
+			json.Marshal(msg)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(msg)
+			return
+
+		}
+	} else {
+		msg := "Product is not found!"
+		json.Marshal(msg)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(msg)
+		return
+
+	}
 }
 
 func AddItemToCart(w http.ResponseWriter, r *http.Request) {
@@ -349,4 +532,134 @@ func AddItemToCart(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+}
+
+func CreateCart(w http.ResponseWriter, r *http.Request) {
+
+	db := config.Connect()
+	defer db.Close()
+	var products models.Cart
+	// var cart []models.Cart
+	var cartitems = []models.Item{}
+	// var addition models.Total
+	// var totalcartvalue int64
+	fmt.Println(cartitems)
+	product := r.FormValue("product")
+	quantity := r.FormValue("quantity")
+	rows, err := db.Query("SELECT productname, quantity FROM inventories WHERE productname=?", product)
+	fmt.Println(rows)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for rows.Next() {
+		err = rows.Scan(&products.Product, &products.Quantity)
+		cartquantity, err1 := strconv.ParseInt(quantity, 0, 0)
+		if err1 != nil {
+			fmt.Println("error while parsing")
+		}
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			if products.Quantity >= cartquantity {
+				_, err2 := db.Exec("INSERT INTO carts(product, quantity) VALUES(?, ?)", product, quantity)
+
+				if err2 != nil {
+					log.Print(err2)
+					return
+				}
+				insertion := "Data Inserted Successfully!"
+				json.Marshal(insertion)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(insertion)
+
+			} else {
+				x := "Data not inserted"
+				json.Marshal(x)
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(x)
+			}
+		}
+	}
+
+}
+
+func GetCart(w http.ResponseWriter, r *http.Request) {
+
+	db := config.Connect()
+	defer db.Close()
+	var products models.Cart
+	var cart []models.Cart
+
+	w.Header().Set("Content-Type", "application/json")
+	message := "All Cart Items ---> "
+	json.Marshal(message)
+	rows, err3 := db.Query("SELECT product, quantity FROM carts")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+	for rows.Next() {
+		err3 = rows.Scan(&products.Product, &products.Quantity)
+		if err3 != nil {
+			fmt.Println(err3.Error())
+		} else {
+			cart = append(cart, products)
+		}
+		fmt.Println(cart)
+	}
+	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode(cart)
+}
+
+func DeleteCart(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+	defer db.Close()
+
+	product := r.FormValue("product")
+	rows, err := db.Query("SELECT product, quantity FROM carts WHERE product=?", product)
+	if product == "" {
+
+		y := "Data Not Found"
+		json.Marshal(y)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(y)
+		return
+	}
+	if rows.Next() == false {
+		z := "Invalid Cart Reference"
+		json.Marshal(z)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(z)
+		return
+
+	}
+	result, err := db.Exec("DELETE FROM carts WHERE product=?", product)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	rows_affected, err := result.RowsAffected()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if rows_affected != 0 {
+		if err != nil {
+			log.Print(err)
+			return
+		} else {
+			msg := "Cart item deleted successfully"
+			json.Marshal(msg)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(msg)
+			return
+
+		}
+	} else {
+		msg := "Product is not found!"
+		json.Marshal(msg)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(msg)
+		return
+
+	}
 }
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -11,12 +11,21 @@ var RegisterShoppingCart = func(router *mux.Router) {
 	router.HandleFunc("/product/{name}", controllers.GetProductByName).Methods("GET")
 	router.HandleFunc("/updateproduct", controllers.UpdateProduct).Methods("PUT")
 	router.HandleFunc("/deleteproduct", controllers.DeleteProduct).Methods("DELETE")
-	router.HandleFunc("/createcart", controllers.AddItemToCart).Methods("POST")
-	// router.HandleFunc("/createcarts", controllers.AddItemsToCart).Methods("POST")
+
 	router.HandleFunc("/createinventory", controllers.CreateInventoryProduct).Methods("POST")
 	router.HandleFunc("/productinventory/{name}", controllers.GetProductByNameInInventory).Methods("GET")
 	router.HandleFunc("/productsinventory", controllers.GetProductsInventory).Methods("GET")
 	router.HandleFunc("/updateproductinventory", controllers.UpdateProductInventory).Methods("PUT")
 	router.HandleFunc("/deleteproductinventory", controllers.DeleteProductInventory).Methods("DELETE")
+
+	router.HandleFunc("/createcategory", controllers.CreateCategory).Methods("POST")
+	router.HandleFunc("/namecategory/{name}", controllers.GetCategoryByName).Methods("GET")
+	router.HandleFunc("/categories", controllers.GetCategories).Methods("GET")
+	router.HandleFunc("/deletecategory", controllers.DeleteCategory).Methods("DELETE")
+
+	router.HandleFunc("/createcart", controllers.CreateCart).Methods("POST")
+	router.HandleFunc("/addcart", controllers.AddItemToCart).Methods("POST")
+	router.HandleFunc("/getcart", controllers.GetCart).Methods("GET")
+	router.HandleFunc("/deletecart", controllers.DeleteCart).Methods("DELETE")
 
 }
