6ea3a2fc020bd3794a4df32d93f866dd395acf57
Sanjana <sanjana.jadhav@searce.com>
2022-12-30 11:55:13 +0530

    Added cases

--- pkg/controllers/Carts/createcart.go
+++ pkg/controllers/Carts/createcart.go
@@ -22,7 +22,7 @@ func CreateCart(w http.ResponseWriter, r *http.Request) {
 
 	db := config.Connect()
 	defer db.Close()
-	var products models.Cart
+
 	// var cart []models.Cart
 	var cartitems = []models.Item{}
 	// var addition models.Total
@@ -30,43 +30,51 @@ func CreateCart(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(cartitems)
 	product := r.FormValue("product")
 	quantity := r.FormValue("quantity")
-	rows, err := db.Query("SELECT productname, quantity FROM inventories WHERE productname=?", product)
-	fmt.Println(rows)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for rows.Next() {
-		err = rows.Scan(&products.Product, &products.Quantity)
-		cartquantity, err1 := strconv.Atoi(quantity)
-		if err1 != nil {
-			fmt.Println("error while parsing")
-		}
+	var products models.Cart
+	fmt.Println(products.Product)
+	if products.Product == "" {
+		x := "Data Cannot be Created"
+		json.NewEncoder(w).Encode(map[string]string{"message": x})
+	} else {
+		rows, err := db.Query("SELECT productname, quantity FROM inventories WHERE productname=?", product)
+		fmt.Println(rows)
 		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			if products.Quantity < 0 {
-				x := map[string]string{"message": "Quantity cannot be Negative"}
-				json.NewEncoder(w).Encode(x)
+			fmt.Println(err)
+		}
+		for rows.Next() {
+			err = rows.Scan(&products.Product, &products.Quantity)
+			cartquantity, err1 := strconv.Atoi(quantity)
+			if err1 != nil {
+				fmt.Println("error while parsing")
 			}
-			if products.Quantity >= cartquantity {
-				_, err2 := db.Exec("INSERT INTO carts(product, quantity) VALUES(?, ?)", product, quantity)
-
-				if err2 != nil {
-					log.Print(err2)
-					return
-				}
-				insertion := "Data Inserted Successfully!"
-				json.Marshal(insertion)
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(insertion)
-
+			if err != nil {
+				fmt.Println(err.Error())
 			} else {
-				x := "Data not inserted"
-				json.Marshal(x)
-				w.Header().Set("Content-Type", "application/json")
-				json.NewEncoder(w).Encode(x)
+				if products.Quantity < 0 {
+					x := map[string]string{"message": "Quantity cannot be Negative"}
+					json.NewEncoder(w).Encode(x)
+				}
+				if products.Quantity >= cartquantity {
+					_, err2 := db.Exec("INSERT INTO carts(product, quantity) VALUES(?, ?)", product, quantity)
+
+					if err2 != nil {
+						log.Print(err2)
+						return
+					}
+					insertion := "Data Inserted Successfully!"
+					json.Marshal(insertion)
+					w.Header().Set("Content-Type", "application/json")
+					json.NewEncoder(w).Encode(insertion)
+
+				} else {
+					x := "Data not inserted"
+					json.Marshal(x)
+					w.Header().Set("Content-Type", "application/json")
+					json.NewEncoder(w).Encode(x)
+				}
 			}
 		}
+
 	}
 
 }
